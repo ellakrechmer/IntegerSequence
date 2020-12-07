e8c3e0fe908f364cc8ce126cb6573431f4c6704b
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-07 12:57:19 -0500

    tested the second constructor (successfully)

--- IntSeqTester.java
+++ IntSeqTester.java
@@ -12,8 +12,18 @@ public class IntSeqTester{
     System.out.println("ArraySequence Testing:");
     int[]nums = {1,3,5,0,-1,3,9};
     IntegerSequence as = new ArraySequence(nums);
+    System.out.println("ArraySequence(array):");
     while(as.hasNext()){
       System.out.print(as.next()+", ");
+    }
+    System.out.println();
+
+    IntegerSequence r1 = new Range(10,20);
+    IntegerSequence scan = new ArraySequence(r1);
+
+    System.out.println("ArraySequence(seq):");
+    while(scan.hasNext()){
+      System.out.print(scan.next()+", ");
     }
     System.out.println();
   }
