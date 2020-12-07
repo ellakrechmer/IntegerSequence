0f1974beb6e78430b47e880b90ba6cfe66515655
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-07 12:45:12 -0500

    tested the class for the first constructor

--- IntSeqTester.java
+++ IntSeqTester.java
@@ -12,10 +12,9 @@ public class IntSeqTester{
     System.out.println("ArraySequence Testing:");
     int[]nums = {1,3,5,0,-1,3,9};
     IntegerSequence as = new ArraySequence(nums);
-
-    System.out.println("ArraySequence(array):");
     while(as.hasNext()){
       System.out.print(as.next()+", ");
     }
+    System.out.println();
   }
 }
